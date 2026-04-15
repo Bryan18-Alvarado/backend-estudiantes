b8d7e796d4ac55eed64313318bed5e5cc823f727
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-04-14 19:20:40 -0600

    review

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: Number(process.env.PORT),
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +21,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen();
+  logger.log(
+    `Microservicio de estudiante corriendo en el puerto ${process.env.PORT}`,
+  );
 }
 bootstrap();
