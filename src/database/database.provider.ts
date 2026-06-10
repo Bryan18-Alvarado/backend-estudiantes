b565e150f3e64a6321c4353f2e9a8518057fb4cc
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-06-09 18:55:28 -0600

    feat: update archive

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -13,7 +13,7 @@ export const DatabaseProvider: DynamicModule = TypeOrmModule.forRootAsync({
     password: configService.get('PASSWORD_DB'),
     database: configService.get('DATABASE'),
     entities: [],
-    autoLoadEntities: configService.get('AUTOLOADENTITIES'),
+    autoLoadEntities: true,
     synchronize: false,
   }),
 });
